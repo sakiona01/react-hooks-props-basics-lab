82dc9daa523bad8da46605e43538ee955873eeed
Shawna Akiona <shawna.akiona@gmail.com>
2021-08-30 22:04:38 -0700

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,23 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  function bio() {
+    if (!props.bio) {
+      return null;
+    } else {
+      return (
+        <p>{props.bio}</p>
+      );
+    }   
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+       {bio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
